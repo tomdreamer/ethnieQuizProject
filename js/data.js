50ce399e591ebef53a8d90d00d5d7353bdd3e7f8
tomdreamer <thomas.lesage@hotmail.com>
2019-01-21 18:07:21 +0100

    First save

---
+++ js/data.js
@@ -0,0 +1,105 @@
+question1 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Kazakhs",
+    pays : "Mongolie",
+    picture : URL("./images/kazakh.jpg"),
+    desription : "Ils sont issus d’une ethnie turque du nord de l’Asie centrale, sont semi-nomades et vivent du pastoralisme, parmi leurs traditions on peut noter, la chasse à l’aigle. Ils portent alors un manteau noir, un chapeau bordé de fourrure, le loovuuz, et des bottes. Ils vivent dans les yourtes richement aménagées avec des étoffes brodées et du feutre. Ce sont de grands consommateurs de viande d’agneau, cheval, qu’ils ont coutume de saler et de sécher pour la conserver. Ils privilégient le lait aigre, qui se conserve mieux et est plus approprié au mode de vie nomade.",
+}
+question2 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Maoris",
+    pays : "Nouvelle-Zélande",
+    picture : URL("./images/moaris.jpg"),
+    desription : "Les Maoris, constituent un peuple indigène fascinant. Ils sont venus en waka (pirogue) de l’île d’Hawaiki en Polynésie centrale. La culture traditionnelle maori s’exprime à travers l’art, les légendes, les tatouages (ta moko), les danses (en particulier le haka censé intimider l’ennemi), les costumes et un grand sens de l’hospitalité. Peuple polythéiste les Maoris vénèrent de nombreux dieux, déesses et esprits. Ils ont adopté un mode de vie occidental, tout en conservant leurs coutumes sociales et culturelles. L’alimentation de base (kai) est constituée d’oiseaux et de poissons que viennent compléter des herbes sauvages et des racines. Ils cultivent dans leurs jardins des calebasses, et des tubercules comme l’igname et la patate douce (kumara). Ils sont à l’heure actuelle quelque 650.000 Maoris en Nouvelle-Zélande.",
+}
+question3 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Mursis",
+    pays : "Éthiopie",
+    picture : URL("./images/mursis"),
+    desription : "Ils vivent non loin de la frontière kenyane, dans la basse vallée de l’Omo. Cette tribu possède son propre idiome, le mursi. Les moyens de subsistance de cette tribu de bergers nomades sont de plus en plus difficiles, suite à de grandes sécheresses, mais aussi la création de parcs nationaux avec leurs clôtures et leurs routes. Les femmes sont connues dans le monde entier pour le labret, plateau d’argile ornant leur bouche. Pour passer de l’enfance à l’âge adulte, les jeunes hommes doivent se soumettre à la cérémonie du « saut du taureau », des bovins sont alignés flancs contre flancs. Chaque homme nu doit effectuer quatre passages sur leurs dos sans tomber. Ils habitent dans des cases faites de roseaux, chaume, branches et brindilles. Ils sont animistes, et croient que tous les éléments de la nature, arbres, pierres… possèdent un esprit. ",
+}
+question4 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Samburus",
+    pays : "Kenya",
+    picture : URL("./images/samburu.jpg"),
+    desription : "Les Samburus sont environ 140.000 et habitent la province de la Rift Valley dans le nord du Kenya. Les steppes leur permettent de faire l’élevage des bovins, chèvres, moutons et dromadaires. Le samburu est un dialecte de la langue nilotique appelée maa, également parlée par les Massaïs. Ce peuple croit à un créateur lointain, un dieu suprême, qu’ils appellent Nkai ou Ngai. Le culte des ancêtres et même la sorcellerie sont très répandus, ils croient aux envoûtements et pratiquent des rituels traditionnels pour obtenir fécondité, protection, guérison… Ils habitent dans des huttes faites de branchages entrelacés, de peaux de bêtes et de boue, un matériel facile à démonter pour le transport. Les guerriers tressent leurs longs cheveux et les teignent avec de l’ocre rouge. Ils sont tous richement parés de colliers, bracelets et boucles d’oreilles en perles très colorés. ",
+}
+question5 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Vanuatuans",
+    pays : "Îles du VanuAtu",
+    picture : "./images/vanuatuans.jgp",
+    desription :"Ils peuplent la république du Vanuatu, en Mélanésie qui regroupe 83 îles du sud-ouest de l’océan Pacifique. Ils pratiquent de grandes cérémonies et procèdent à des échanges d’aliments : taros, ignames, kava, volailles, cochons et poulets. Le festival de Toka est l’une des manifestations traditionnelles qui dure trois jours. La danse occupe une grande place pour les Vanuatuans. Le namba est un étui pénien traditionnel confectionné avec de l’écorce ou les feuilles du pandanus. Les femmes portent des pagnes confectionnés avec des feuilles, des nattes tissées, des fibres d’hibiscus. Le plat cérémonial le laplap est un gâteau réalisé à partir de racines râpées ou de bananes plantain mélangées à de la noix de coco, des légumes et des viandes, enveloppées dans des feuilles et cuites durant des heures dans un four d’argile traditionnel. ",
+}
+question6 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "papou",
+    pays : "Papouasie-Nouvelle-Guinée",
+    picture : URL("./images/papou.jpg"),
+    desription : "Les papous vivent dans les hautes terres de Papouasie-Nouvelle-Guinée et l’on pense qu’ils ont migré vers l’île il y a plus de 45000 années. C’est la plus importante tribu de cette île, surnommés les « Hommes perruques », célèbres impressionnantes perruques confectionnées avec leurs propres cheveux. Elles sont ornées de plumes d’oiseaux de paradis, de perroquets. Coquillages, perles, défenses de cochon, crânes de calao et feuilles sont également utilisés. Traditionnellement animistes, ils procèdent à des offrandes suivant des rituels établis afin d’apaiser les esprits de leurs ancêtres. Ils voient dans la maladie et le malheur l’oeuvre de la sorcellerie. Ils sont très respectueux des merveilles de la nature. Les hommes aident à défricher les terres, le reste incombe aux femmes, qui sont des agricultrices hors pair. Elles cultivent des patates douces, du blé et diverses variétés de chou et de manioc. Ils vivent dans des huttes de chaume, deux à quatre par communauté. Les femmes dorment dans une hutte et les hommes dans une autre. ",
+}
+question7 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Samburus",
+    pays : "Kenya",
+    picture : URL("./images/samburu.jpg"),
+    desription : "Ils habitent dans la Rift Valley au nord du Kenya. Le mariage est une succession de rituels très élaborés. Ils accordent une grande importance aux cadeaux émanant du fiancé (deux peaux de chèvres, une paire de boucles d’oreille en cuivre, une calebasse, et un mouton). La fertilité est très importante, et les femmes sans enfants sont tournées en ridicule. Un rituel de fertilité consiste à  placer une statuette en terre devant la hutte d’une femme et une semaine plus tard le mari invite les voisins à partager un taureau. Les femmes portent la shuka traditionnelle, une pièce d’étoffe aux couleurs vives dont elles enveloppent leurs corps. Elles sont parées de riches colliers de perle très colorés, de boucle d’oreille. Le lait et le sang de leurs vaches constituent la base de leur alimentation, mais ils la complètent par des racines et des légumes qu’ils trouvent dans la terre et en accommodent leur soupe. ",
+}
+question8 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Yalis",
+    pays : "Indonésie";
+    picture : URL("./images/kazakh.jpg"),
+    desription : "Les Yalis, « Seigneurs de la Terre », vivent au coeur des monts Jayawijaya, dans la province indonésienne de Papouasie. Le koteka, étui pénien long et effilé, est l’une des particularités de cette tribu. Il est fait dans une coloquinte et fait partie du costume traditionnel, ainsi que les ornements faciaux et corporels faits en os, dents de chien ou de porc. Ils pratiquent la polygamie. Les Yalis construisent des huttes ovales ou rondes en bois, coiffées d’épais toits de chaume. Les hommes, les femmes et les enfants dorment dans des huttes séparées (honai). Le sagou constitue l’alimentation de base, mais elle est complétée par des larves, des cochons sauvages, des serpents, des casoars. En guise de légumes ils consomment des feuilles de palmier, des fougèreset les fruits de l’arbre à pain.",
+}
+question9 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Kayapo",
+    pays : "Brésil" ,
+    picture : URL("./images/cacique.jpg"),
+    desription :"es Kayapos (également orthographié Kayapós, Caiapós ou encore Kaipo) sont un peuple indigène du Brésil et l'une des 17 nations résidant dans la vallée du Rio Xingu, en grande majorité dans le Territoire indigène de Kayapó, au sud du bassin de l'Amazone, dans les plaines des États du Mato Grosso et du Pará. Ils parlent le kayapo, l'une des langues jê-kaingang. La frontière de leur réserve a été officiellement délimitée lors d’un décret présidentiel en 1993 lors de la tournée mondiale du Chef Raoni accompagné du chanteur Sting et du cinéaste Jean-Pierre Dutilleux. Environ 7 096 Kayapos sont comptés en 2003, dispersés dans diverses tribus (Kayapo Gorotire, Xicrin, Metyktire, Kuben-Kran Ken ou Txucarramae) qui vivent sur des territoires indigènes des États brésiliens du Pará et du Mato Grosso.Ils mènent un combat médiatique depuis 1973, lorsque le cinéaste belge Jean-Pierre Dutilleux réalisa un documentaire sur les Txucarramae intitulé Raoni. Le chanteur Sting s'est intéressé à leur cause",
+}
+question10 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Nénètses",
+    pays : "",
+    picture : URL("./images/kazakh.jpg"),
+    desription : "Ethnie nomade d’éleveurs de rennes de l’Arctique sibérien, ils occupent en majorité la péninsulede Yamal, au nord-est de l’Oural. Ils vivent depuis plus d’un millénaire dans en un des milieux les plus inhospitaliers de la planète, -50 °C en hiver +35 °C en été. Les rennes occupent une place prépondérante dans la vie et les traditions des Nénètses, valeur marchande, nourriture, protection, habillement… Les rennes sont vénérés. Les hommes portent un malitsa, sorte de manteau à capuche en cuir, doublé de fourrure. Dans les conditions extrêmes, ils enfilent une autre couche de fourrure de renne, le gus. La yagushka des femmes est un vêtement à double épaisseur. Les Nénètses portent de longues cuissardes avec une botte intérieure le tobaki et une botte extérieure le kisy. Ils vivent dans les tchoums, tentes familiales faites en peaux de rennes. Leur boisson favorite est le thé noir de Ceylan, et ils se nourrissent de viandes de rennes, de poissons séchés. ",
+}
+question11 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Samis",
+    pays: "Norvège";
+    picture : URL("./images/Sami.jpg"),
+    desription : "Les Samis (aussi désignés sous le nom de Lapons) sont un peuple autochtone d'une zone qui couvre le nord de la Suède, de la Norvège et de la Finlande ainsi que la péninsule de Kola en Russie connue sous le nom de Laponie. Leur nom, Saami dans leur propre langue, est également parfois écrit « Sámi », « Sames », « Samés » ou encore « Sâmes ». Les samis parlent des langues sames. Ce peuple est souvent nommé « Lapons » mais ce terme est non seulement un terme étranger mais aussi originellement péjoratif, issu de la racine lapp qui signifie porteur de haillons en suédois3. De même, ils appellent leurs terres ancestrales Sápmi et non Laponie. Les activités traditionnelles des Samis étaient autrefois la pêche et l'élevage de rennes, mais aujourd'hui seule une minorité des 85 000 Sames en vit encore. ",
+}
+question12 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Yi",
+    pays : "Chine",
+    picture : URL("./images/yi.jpg"),
+    desription : "Les Yi sont un peuple tibéto-birman majeur de la Chine du Sud, établi en grande majorité au Yunnan et dans la région des Da Liangshan, au Sud du Yunnan, avec quelques poches au Guangxi et au Nord Vietnam. Nous traversons au cours de cette étape les Da Liangshan, c’est à dire les « Grandes Montagnes Froides », région de montagnes isolées, que contourne le grand fleuve Yangtsé avant d’aller s’étaler dans les grandes plaines du Sichuan, qui n’est pratiquement jamais proposée dans des parcours de Tour Operators. Cette région abrite une importante population Yi, autrefois connue sous le nom générique de Lolo, notamment par des explorateurs Français du début du 20ème siècle, dont le commandant d’Ollone, qui leur a consacré un ouvrage intitulé « Les derniers barbares ».",
+}
+question13 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Inuit",
+    pays : "Canada",
+    picture : URL("./images/kazakh.jpg"),
+    desription : "Les Inuits sont un groupe de peuples autochtones partageant des similarités culturelles et une origine ethnique commune vivant dans les régions arctiques de l'Amérique du Nord. Il y a environ 150 000 Inuits vivant au Groenland, au Canada et aux États-Unis. Bien que le Conseil circumpolaire inuit regroupe également les Yupiks de l'Alaska et de la Sibérie, ceux-ci ne sont pas des Inuits dans le sens d'une descendance thuléenne. Les Inuits ne sont pas considérés comme des Amérindiens puisque leurs ancêtres seraient venus en Amérique plusieurs millénaires après l'arrivée des Paléoasiatiques, les ancêtres des Amérindiens4. En fait, les Inuits sont davantage similaires aux peuples habitant les régions arctiques asiatiques qu'aux peuples amérindiens5. Il ne faut pas non plus confondre les Inuits avec les Innus qui sont un peuple amérindien vivant dans la forêt boréale canadienne du Nord-Est du Québec et du Labrador. Historiquement, les Inuits étaient un peuple de chasseurs nomades. De nos jours, si la plupart des Inuits sont devenus sédentaires, une grande partie vit encore de la chasse et de la pêche. ",
+}
+question14 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Hani",
+    pays : "Birmanie",
+    picture : URL("./images/hani.jpg"),
+    desription :"C'est la première fois que nous rencontrons l'ethnie Hani, peuple de langue tibéto-birmane comme les Yi, très nombreux au Sud Yunnan (environ 1,3 million), le long des frontières avec le Vietnam, le Laos et la Birmanie. Appelés Iko au Laos, mais aussi Akha en Thaïlande et en Birmanie, ils sont répartis en plusieurs sous-groupes aux traditions vestimentaires complètement différentes. Dans la région de Yuanyang et Jinping ce sont les Hani qui sont l'ethnie la plus nombreuse, mais répartie en nombreux sous-groupes, avec des tenues vestimentaires et coiffes très différentes les uns des autres, surtout près de la frontière vietnamienne, entre Pinghe et Nafa. Les costumes sont aussi très différents de ceux que l'on trouve au Xishuangbanna, au Laos et en Birmanie. Les femmes Hani de la région portent des pantalons teints à l’indigo et une tunique brodée.",
+}
+question15 ={
+    question : "Where does this ethnie come's from?",
+    nameEthnie : "Kogi",
+    pays : "colombie";
+    picture : URL("./images/kazakh.jpg"),
+    desription : "Pour les indigènes vivant dans la Sierra Nevada de Santa Marta, ces montagnes représentent le "cœur du monde. Ainsi, la Sierra Nevada de San Marta maintient l'équilibre spirituel et écologique de toute la planète. La mythologie des Indiens Kogis conte qu'ils sont les frères aînés de l'humanité. Les étrangers, en particulier ceux d'Occident, sont appelés les "petits frères". La légende veut que ces petits frères aient été bannis, autrefois, du cœur du monde à cause des infractions qu'ils ont commises. Ils doivent donc poursuivre leur route et subir les châtiments de leurs méfaits12. Leurs rituels et traditions sont caractérisés par un rapport très fort et très sensitif à la Terre. Ils se sentent encore de nos jours « gardiens de la Terre » qu'ils considèrent et traitent comme « sacrée ». Ils sont capables, selon leurs dires, de percevoir les lieux où la Terre est « vivante » et ceux où elle est morte.",
+}
